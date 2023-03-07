fc22517f05a85ea85a8845cd347b9eac05cd8541
Barrie Treloar <baerrach@gmail.com>
2023-03-07 12:25:30 +1030

    Update indexing scripts

--- indexing/create_index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename="words"
-while read -r line
-do
-  ./create_index_for_word.sh "${line//[$'\r\n']}"
-done < "$filename"
--- indexing/create_index_for_word.sh
+++ indexing/create_index_for_word.sh
@@ -11,8 +11,9 @@ if [ $# -ne 1 ]; then
    exit
 fi
 
-if [ ! -d "tmp" ]; then
-    mkdir tmp
+TMP_DIR="tmp"
+if [ ! -d "${TMP_DIR}" ]; then
+    mkdir "${TMP_DIR}"
 fi
 
 INDEX_DIR="../index"
@@ -21,7 +22,7 @@ if [ ! -d "${INDEX_DIR}" ]; then
 fi
 
 INDEX_WORD=$1
-FILE_INDEX_FULL_SCAN="tmp/_index.full_scan"
+FILE_INDEX_FULL_SCAN="${TMP_DIR}/_index.full_scan"
 FILE_INDEX="${INDEX_DIR}/${INDEX_WORD}.md"
 
 rm "${FILE_INDEX_FULL_SCAN}"
@@ -30,3 +31,6 @@ touch "${FILE_INDEX_FULL_SCAN}"
 find .. -type f -name "*.vbs" -not -path indexing -print0 | xargs -0 grep --with-filename --only-matching "${INDEX_WORD}" >> "${FILE_INDEX_FULL_SCAN}"
 echo "# ${INDEX_WORD}" > "${FILE_INDEX}"
 cat ${FILE_INDEX_FULL_SCAN} | sed 's/\.\.\///' | sort | uniq | awk 'BEGIN {FS=OFS=":"} {printf "* [%s](<../%s>)\n", $1, $1}' >> "${FILE_INDEX}"
+
+rm "${FILE_INDEX_FULL_SCAN}"
+rmdir "${TMP_DIR}"
